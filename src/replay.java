e06ecfd5d99de3060c8d3f8a5954c9ec903445d3
Mihail1231234 <bibikovm1501@gmail.com>
2022-06-05 17:47:31 +0600

    Initial commit

---
+++ src/replay.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class replay {
+    public static void main(String[] args) {
+        System.out.println("Введите первое число");
+
+        Scanner a = new Scanner(System.in);
+        int number1 = a.nextInt();
+
+        System.out.println("Введите знак");
+
+        Scanner b = new Scanner(System.in);
+        String scannerOne = b.next();
+
+        System.out.println("Введте второе число");
+
+        Scanner c = new Scanner(System.in);
+        int number2 = c.nextInt();
+
+        int d = number1+number2;
+        int e = number1-number2;
+        int f = number1*number2;
+        int g = number1/number2;
+
+        if (){
+            System.out.println(e);
+
+        }else if (-){
+            System.out.println(e);
+        }else if (*){
+            System.out.println(f);
+        }else if (/){
+
+        }
+
+        }
+
+    }
+
+
